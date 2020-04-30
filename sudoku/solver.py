c0d45ca9f9fbb4aede722b0672b77eb3568a8ac2
Schlendrikovic <julian94Scherner>
2020-04-30 11:53:17 +0200

    Refactor and include doc strings

--- sudoku/solver.py
+++ sudoku/solver.py
@@ -1,113 +1,184 @@
-import math
+"""Sudoku solver"""
 import numpy as np
-import sys 
-import ipdb
 
-def solve(grid: list):
-	print("Start Solver:\n")
-	solution = []
-	grid = np.array(grid)
 
-	ridx = 0
-	cidx = 0
-	position = (ridx, cidx)
+def solve(grid: list) -> list:
+    """Solve the given sudoku grid using backtracking (brute force).
 
-	
-	open_positions = np.where(grid==0)
+    See: https://en.wikipedia.org/wiki/Sudoku_solving_algorithms
 
-	pointer = 0
-	
-	while pointer < len(open_positions[0]):
-		position = open_positions[0][pointer],open_positions[1][pointer]
+    Parameters
+    ----------
+    grid : list
+        Sudoku grid with blanks
 
-		if find_candidate(grid, position)[0]:
-			candidate = find_candidate(grid, position)[1]
-			grid[position] = candidate
-			pointer += 1
-		
-		else:
-			negative_pointer = pointer - 1
+    Returns
+    -------
+    grid : list
+        Solved sudoku grid
+    """
 
-			while negative_pointer >=0:
-			
-				position = open_positions[0][negative_pointer],open_positions[1][negative_pointer]
-				
-				if find_increment(grid, position)[0]:
-					candidate = find_increment(grid, position)[1]
-					grid[position] = candidate
-					pointer = negative_pointer + 1	
-					break
+    grid = np.array(grid)
+    print(f"\nStart Solving:\n {grid}\n")
 
-				grid[position] = 0
-				negative_pointer -= 1
+    open_positions = np.where(grid == 0)
 
-				if (negative_pointer == -1):
-					raise 
+    candidate_pointer = 0
+    while candidate_pointer < len(open_positions[0]):
 
-	print(f"Valid Solution:\n {grid}")
-	return grid
+        position = get_position(candidate_pointer, open_positions)
+        candidate = find_candidate(grid, position)
 
-def find_candidate(grid , position: tuple):
-	for candidate in np.arange(1,10,1):
-		if valid_candidate(candidate, grid, position):
-			return (True , candidate)
+        if isinstance(candidate, int):
+            grid[position] = candidate
+            candidate_pointer += 1
+        else:
 
-	return (False, None)
+            update_pointer = candidate_pointer - 1
 
-def find_increment(grid, position):
-	for candidate in np.arange(grid[position],10,1):
-		if valid_candidate(candidate, grid, position):
-			return (True, candidate)
-	
-	return (False, None)			
+            while update_pointer >= 0:
+
+                position = get_position(update_pointer, open_positions)
+                update = find_increment(grid, position)
+
+                if isinstance(update, int):
+                    grid[position] = update
+                    candidate_pointer = update_pointer + 1
+                    break
+
+                grid[position] = 0
+                update_pointer -= 1
+
+                if update_pointer == -1:
+                    raise
+
+    print(f"Valid Solution:\n {grid}")
+    return grid
+
+
+def get_position(pointer: int, open_positions: list) -> tuple:
+    """Return the current position indizes (row,col)
+
+    Parameters
+    ----------
+    pointer : int
+        Points to the current open position
+    open_positions : list
+        List of all open positions that need to be filled with a
+        number between 1 and 9
+
+    Returns
+    -------
+    tuple
+        The current position (row,col)
+    """
+    return open_positions[0][pointer], open_positions[1][pointer]
+
+
+def find_candidate(grid: list, position: tuple) -> int:
+    """Retrieve a possible candidate for the given position
+
+    Parameters
+    ----------
+    grid : list
+        Sudoku Grid
+    position : tuple
+        Current position on grid (row,col)
+
+    Returns
+    -------
+    int
+        A possible candidate
+    """
+    for candidate in np.arange(1, 10, 1):
+        if valid_candidate(candidate, grid, position):
+            return int(candidate)
+    return None
+
+
+def find_increment(grid: list, position: tuple) -> int:
+    """Retrieve the first possible increment for the given position
+
+    Parameters
+    ----------
+    grid : list
+        Sudoku Grid
+    position : tuple
+        Current position on grid (row,col)
+
+    Returns
+    -------
+    int
+        A possible increment
+    """
+    for update in np.arange(grid[position], 10, 1):
+        if valid_candidate(update, grid, position):
+            return int(update)
+
+    return None
 
 
 def valid_candidate(candidate: int, grid: list, position: tuple):
-	"""Check that the candidate does not violate the freuqnecy condition
-	in the corresponding row, column and box.
+    """Check that the candidate does not violate the freuqnecy condition
+    in the corresponding row, column and box.
 
-	Parameters
-	----------
-	candidate : int
-	    Description
-	grid : list
-	    Description
-	position : tuple
-	    (row, col)
-	"""
+    Parameters
+    ----------
+    candidate : int
+        Description
+    grid : list
+        Sudoku Grid
+    position : tuple
+        Current position on grid (row,col)
 
-	row = grid[position[0],:]
-	col = grid[:,position[1]]
-	box = get_grid(grid, position)
+    Returns
+    -------
+    TYPE
+        Description
+    """
 
-	if (candidate in row) or (candidate in col) or (candidate in box):
-		return False 
-	
-	return True
-		
+    row = grid[position[0], :]
+    col = grid[:, position[1]]
+    box = get_box(grid, position)
+
+    if (candidate in row) or (candidate in col) or (candidate in box):
+        return False
+
+    return True
 
 
-def get_grid(grid: list, position: tuple):
-	"""Retrive the grid to the corresponding position.	
-	The common grid is 9x9 and there are 3 boxes vertically and horizontally
+def get_box(grid: list, position: tuple):
+    """Retrive the grid to the corresponding position.
+    The common grid is 9x9 and there are 3 boxes vertically and horizontally
 
-	grid = ( 
-		Box1 Box2 Box3
-		Box4 Box5 Box6
-		Box7 Box8 Box9	
-	)
+    grid = (
+        Box1 Box2 Box3
+        Box4 Box5 Box6
+        Box7 Box8 Box9
+    )
 
-	"""
+    Parameters
+    ----------
+    grid : list
+        Sudoku Grid
+    position : tuple
+        Current position on grid (row,col)
 
-	grid = np.array(grid)
+    Returns
+    -------
+    list
+        The corresponding box of the current position on the grid
 
-	box_h = position[0] // 3
-	box_v = position[1]    // 3
+    """
 
-	ridx_from = 3 * box_h
-	ridx_to  = 3 * box_h + 3 
-	cidx_from = 3 * box_v
-	cidx_to = 3 * box_v + 3 
+    grid = np.array(grid)
 
-	return grid[ridx_from:ridx_to , cidx_from:cidx_to]
+    box_h = position[0] // 3
+    box_v = position[1] // 3
 
+    ridx_from = 3 * box_h
+    ridx_to = 3 * box_h + 3
+    cidx_from = 3 * box_v
+    cidx_to = 3 * box_v + 3
+
+    return grid[ridx_from:ridx_to, cidx_from:cidx_to]
--- tests/test_helper.py
+++ tests/test_helper.py
@@ -1,45 +1,42 @@
-import pytest 
-from sudoku.solver import get_grid
-
-def test_get_grid():
-
-	grid = [
-		[7,2,6,4,9,3,8,1,5],
-		[3,1,5,7,2,8,9,4,6],
-		[4,8,9,6,5,1,2,3,7],
-		[8,5,2,1,4,7,6,9,3],
-		[6,7,3,9,8,5,1,2,4],
-		[9,4,1,3,6,2,7,5,8],
-		[1,9,4,8,3,6,5,7,2],
-		[5,6,7,2,1,4,3,8,9],
-		[2,3,8,5,7,9,4,6,1],
-	]
-
-	expected_result = [
-		[7,2,6],
-		[3,1,5],
-		[4,8,9],
-	]
-
-	assert (expected_result == get_grid(grid, (0,0))).all()
-	assert (expected_result == get_grid(grid, (0,1))).all()
-	assert (expected_result == get_grid(grid, (2,0))).all()
-
-	expected_result = [
-		[8,3,6],
-		[2,1,4],
-		[5,7,9],
-	]
-
-	assert (expected_result == get_grid(grid, (8,4))).all()
-
-	expected_result = [
-		[5,7,2],
-		[3,8,9],
-		[4,6,1],
-	]
-
-	assert (expected_result == get_grid(grid, (8,8))).all()
+from sudoku.solver import get_box
 
 
+def test_get_box():
 
+    grid = [
+        [7, 2, 6, 4, 9, 3, 8, 1, 5],
+        [3, 1, 5, 7, 2, 8, 9, 4, 6],
+        [4, 8, 9, 6, 5, 1, 2, 3, 7],
+        [8, 5, 2, 1, 4, 7, 6, 9, 3],
+        [6, 7, 3, 9, 8, 5, 1, 2, 4],
+        [9, 4, 1, 3, 6, 2, 7, 5, 8],
+        [1, 9, 4, 8, 3, 6, 5, 7, 2],
+        [5, 6, 7, 2, 1, 4, 3, 8, 9],
+        [2, 3, 8, 5, 7, 9, 4, 6, 1],
+    ]
+
+    expected_result = [
+        [7, 2, 6],
+        [3, 1, 5],
+        [4, 8, 9],
+    ]
+
+    assert (expected_result == get_box(grid, (0, 0))).all()
+    assert (expected_result == get_box(grid, (0, 1))).all()
+    assert (expected_result == get_box(grid, (2, 0))).all()
+
+    expected_result = [
+        [8, 3, 6],
+        [2, 1, 4],
+        [5, 7, 9],
+    ]
+
+    assert (expected_result == get_box(grid, (8, 4))).all()
+
+    expected_result = [
+        [5, 7, 2],
+        [3, 8, 9],
+        [4, 6, 1],
+    ]
+
+    assert (expected_result == get_box(grid, (8, 8))).all()
